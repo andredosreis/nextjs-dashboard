9c82ee9c2fac2de88072f0b3987008f3bacdae72
Andre Dos Reis <andredosreis@gmail.com>
2025-08-04 20:07:43 +0100

    mundança na rota

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const { handlers, auth } = NextAuth({
+const { handlers, auth } = NextAuth({
   secret: process.env.AUTH_SECRET,
   providers: [
     CredentialsProvider({
